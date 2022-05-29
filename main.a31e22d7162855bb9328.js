0e33f5a46019c18dd07eab25117b06f7d5054cd3
Anna Sharkova <3yucpgxkr8cn@mail.ru>
2022-05-29 23:22:46 +0300

    Webpack2: разделила на 2 файла

---
+++ main.a31e22d7162855bb9328.js
@@ -0,0 +1,77 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+
+;// CONCATENATED MODULE: ./src/image/main-1.5x.png
+const main_1_5x_namespaceObject = __webpack_require__.p + "image/main-1.5x.png";
+;// CONCATENATED MODULE: ./src/image/biography-1.5x.png
+const biography_1_5x_namespaceObject = __webpack_require__.p + "image/biography-1.5x.png";
+;// CONCATENATED MODULE: ./src/image/sinitta-leunen.png
+const sinitta_leunen_namespaceObject = __webpack_require__.p + "image/sinitta-leunen.png";
+;// CONCATENATED MODULE: ./src/image/avery-klein.png
+const avery_klein_namespaceObject = __webpack_require__.p + "image/avery-klein.png";
+;// CONCATENATED MODULE: ./src/image/dawid-liberadzki.png
+const dawid_liberadzki_namespaceObject = __webpack_require__.p + "image/dawid-liberadzki.png";
+;// CONCATENATED MODULE: ./src/image/designer_jess_wonder.png
+const designer_jess_wonder_namespaceObject = __webpack_require__.p + "image/designer_jess_wonder.png";
+;// CONCATENATED MODULE: ./src/image/designer_kameron_clein.png
+const designer_kameron_clein_namespaceObject = __webpack_require__.p + "image/designer_kameron_clein.png";
+;// CONCATENATED MODULE: ./src/image/designer_louisse_red.png
+const designer_louisse_red_namespaceObject = __webpack_require__.p + "image/designer_louisse_red.png";
+;// CONCATENATED MODULE: ./src/image/keghan-crossland.png
+const keghan_crossland_namespaceObject = __webpack_require__.p + "image/keghan-crossland.png";
+;// CONCATENATED MODULE: ./src/index.js
+/*css*/
+
+
+
+/*image*/
+
+
+
+
+
+
+
+
+
+
+/******/ })()
+;
